32ec40126b913285673f1f605787bd93abe9d7ef
arpandottech <arpanpadamani2000@gmail.com>
2026-01-11 19:45:36 +0530

    Fix API URL for production

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
     // Replace this with your computer's local IP address
-    API_URL: "http://192.168.1.14:5000/api",
-    META_PIXEL_ID: "YOUR_PIXEL_ID_HERE" // Paste your Pixel ID here
+    API_URL: "https://techoragadgets.store/api"
 };
 
 export default config;
